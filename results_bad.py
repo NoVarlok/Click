43b4d308255e00eda624ef30a3303781ffbbaa7f
NoVarlok <yakhtin.leonid@yandex.ru>
2024-05-25 00:41:14 +0300

    Change measuring script

--- results_bad.py
+++ results_bad.py
@@ -1,9 +1,11 @@
 import os
 import numpy as np
+import pandas as pd
 import json
 import multiprocessing as mp
 import argparse
 import csv
+from tabulate import tabulate
 
 
 def is_negative(x):
@@ -26,7 +28,6 @@ def process(infer_data_path):
     if os.path.exists(f'{infer_data_path}/pred_list.txt'):
         preds = [json.loads(e) for e in open(f'{infer_data_path}/pred_list.txt')]
         preds = np.array(preds).reshape(-1, multiple, 2).transpose(2, 0, 1)
-        #max_negative = preds[0].max(axis=-1).mean()
         emp_prob = (is_negative(preds).sum(axis=-1) > 0).mean()
         stats_line.extend([str(emp_prob)])
     else:
@@ -56,19 +57,33 @@ def main(args):
         save_name = './results_bad.txt'
     else:
         save_name = './results_bad_test.txt'
-    stats_writer = csv.writer(open(save_name, 'w'), delimiter='\t')
-
-    pool = mp.Pool(mp.cpu_count())
-    for stats_line in pool.imap(process, args.infer_data_paths):
-        stats_writer.writerow(stats_line)
+    df_dict = {
+        'Method': [],
+    }
+    for column in args.columns:
+        df_dict[column] = []
+    for path, name in zip(args.infer_data_paths, args.names):
+        stats = process(path)
+        df_dict['Method'].append(name)
+        for column, value in zip(args.columns, stats):
+            df_dict[column].append(value)
+    df = pd.DataFrame.from_dict(df_dict)
+    # df.to_csv(save_name, index=False)
+    with open(save_name, 'w') as fout:
+        # print(tabulate(df, headers='keys', tablefmt='pipe', stralign='left', showindex=False), file=fout)
+        print(df.to_markdown(index=False,), file=fout)
 
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--infer_data_paths', type=str, nargs='+')
+    parser.add_argument('--names', type=str, nargs='+')
+    parser.add_argument('--columns', type=str, nargs='+')
     parser.add_argument('--test', action='store_true')
 
     args = parser.parse_args()
+    print('infer_data_paths:', len(args.infer_data_paths))
+    print('names:', len(args.names))
 
     main(args)
